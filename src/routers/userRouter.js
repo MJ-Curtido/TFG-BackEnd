6520b54464b9c4eae108052afde710ed3f00b764
Manu <manu17pc@gmail.com>
2023-05-16 23:02:38 +0200

    Actualización de registro evitando coincidencias

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const User = require("../models/user");
+const express = require('express');
+const User = require('../models/user');
 const router = new express.Router();
-const auth = require("../middleware/auth");
+const auth = require('../middleware/auth');
 
 //registrar usuario
-router.post("/users/signin", async (req, res) => {
+router.post('/users/signin', async (req, res) => {
     const user = new User(req.body);
+
+    const existingUser = await User.findOne({ $or: [{ email: user.email }, { telephone: user.telephone }] });
+
+    if (existingUser) return res.status(400).send({ error: 'Some data already exists.' });
 
     try {
         await user.save();
         const token = await user.generateAuthToken();
         res.status(201).send({ user: user, token });
     } catch (e) {
-        res.status(400).send({error: e.message});
+        res.status(400).send({ error: e.message });
     }
 });
 
 //iniciar sesión
-router.post("/users/login", async (req, res) => {
+router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthToken();
@@ -29,32 +33,32 @@ router.post("/users/login", async (req, res) => {
 });
 
 //cerrar sesión
-router.get("/users/logout", auth, async (req, res) => {
+router.get('/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token;
         });
         await req.user.save();
 
-        res.send({ message: "Log out successfully." });
+        res.send({ message: 'Log out successfully.' });
     } catch (e) {
         res.status(500).send({ error: e.message });
     }
 });
 
 //obtener usuario
-router.get("/users/me", auth, async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
 //actualizar usuario
-router.patch("/users/me", auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
-    const allowedUpdates = ["name", "email", "password", "birthdate", "telephone"];
+    const allowedUpdates = ['name', 'email', 'password', 'birthdate', 'telephone'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
     if (!isValidOperation) {
-        return res.status(400).send({ error: "Invalid updates" });
+        return res.status(400).send({ error: 'Invalid updates' });
     }
 
     try {
@@ -63,18 +67,18 @@ router.patch("/users/me", auth, async (req, res) => {
 
         res.send(req.user);
     } catch (e) {
-        res.status(400).send({ error: e.message});
+        res.status(400).send({ error: e.message });
     }
 });
 
 //eliminar usuario
-router.delete("/users/me", auth, async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.user._id);
 
         res.send(req.user);
     } catch (e) {
-        res.status(500).send({error: e.message});
+        res.status(500).send({ error: e.message });
     }
 });
 
