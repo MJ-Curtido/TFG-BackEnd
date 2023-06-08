0d22d4715ee9e26b2c437cb36c4d90ed4e6f32d6
Manu <manu17pc@gmail.com>
2023-06-08 20:57:50 +0200

    Get user by id

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -54,6 +54,19 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
+//obtener usuario por id
+router.get('/users/:id', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user) return res.status(404).send({ error: 'User not found.' });
+
+        res.send(user);
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
+});
+
 //actualizar usuario
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
