d7767d89cdbe50fe94bea8fd5ea410b324a8863e
Manu <manu17pc@gmail.com>
2023-06-08 17:27:17 +0200

    Populate de los usuarios de las reviews

--- src/routers/recipeRouter.js
+++ src/routers/recipeRouter.js
@@ -4,6 +4,8 @@ const auth = require('../middleware/auth');
 const router = new express.Router();
 const pageSize = 10;
 
+//TODO: Hacer el método para poner una reseña a una receta con patch, ya sea con el que ya está creado o con uno nuevo
+
 function sortByValuation(recipes) {
     return recipes.sort((a, b) => b.valuation - a.valuation);
 }
@@ -17,7 +19,7 @@ router.post('/recipes/create', auth, async (req, res) => {
 
     try {
         await recipe.save();
-        recipe = await Recipe.findById(recipe._id).populate('author');
+        recipe = await Recipe.findById(recipe._id).populate('author').populate('reviews.user');
         res.status(201).send(recipe);
     } catch (e) {
         res.status(400).send({ error: e.message });
@@ -36,7 +38,7 @@ router.get('/recipes/me', auth, async (req, res) => {
             return res.status(404).send({ error: "You haven't created any recipe." });
         }
 
-        const recipes = await Recipe.find({ author: req.user._id }).populate('author');
+        const recipes = await Recipe.find({ author: req.user._id }).populate('author').populate('reviews.user');
 
         res.send({
             recipes: sortByValuation(recipes).slice(skip, skip + pageSize),
@@ -59,7 +61,7 @@ router.get('/recipes/available', auth, async (req, res) => {
             return res.status(404).send({ error: 'There are no available recipes.' });
         }
 
-        const recipes = await Recipe.find({ author: { $ne: req.user._id } }).populate('author');
+        const recipes = await Recipe.find({ author: { $ne: req.user._id } }).populate('author').populate('reviews.user');
 
         res.send({
             recipes: sortByValuation(recipes).slice(skip, skip + pageSize),
@@ -84,7 +86,7 @@ router.get('/recipes/search/:search', auth, async (req, res) => {
 
         const skip = (page - 1) * pageSize;
 
-        const recipes = await Recipe.find({ author: { $ne: req.user._id } }).populate('author');
+        const recipes = await Recipe.find({ author: { $ne: req.user._id } }).populate('author').populate('reviews.user');
         let filteredRecipes = sortByValuation(recipes.filter((recipe) => recipe.title.toLowerCase().includes(search.toLowerCase())));
         const remainingRecipes = recipes.filter((recipe) => !filteredRecipes.includes(recipe));
         const filteredRecipesByIng = remainingRecipes.filter((recipe) => recipe.ingredients.some((ing) => ing.name.toLowerCase().includes(search.toLowerCase())));
@@ -122,7 +124,7 @@ router.get('/recipes/user/:id', auth, async (req, res) => {
             return res.status(404).send({ error: 'User has not created any recipe.' });
         }
 
-        const recipes = await Recipe.find({ author: id }).populate('author');
+        const recipes = await Recipe.find({ author: id }).populate('author').populate('reviews.user');
 
         res.send({
             recipes: sortByValuation(recipes).slice(skip, skip + pageSize),
@@ -136,7 +138,7 @@ router.get('/recipes/user/:id', auth, async (req, res) => {
 //obtener receta por id
 router.get('/recipes/:id', auth, async (req, res) => {
     try {
-        const recipe = await Recipe.findById(req.params.id).populate('author');
+        const recipe = await Recipe.findById(req.params.id).populate('author').populate('reviews.user');
 
         if (!recipe) {
             return res.status(404).send({ error: 'Recipe not found.' });
@@ -148,10 +150,25 @@ router.get('/recipes/:id', auth, async (req, res) => {
     }
 });
 
+//obtener si una receta es mía
+router.get('/recipes/me/:id', auth, async (req, res) => {
+    try {
+        const recipe = await Recipe.findOne({ _id: req.params.id, author: req.user._id });
+
+        if (!recipe) {
+            return res.send({ isMine: false });
+        }
+
+        return res.send({ isMine: true });
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
+});
+
 //modificar receta
 router.patch('/recipes/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
-    const allowedUpdates = ['title', 'images', 'description', 'ingredients', 'steps', 'price', 'author', 'valuation', 'reviews'];
+    const allowedUpdates = ['title', 'images', 'description', 'ingredients', 'steps', 'price', 'reviews'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
     if (!isValidOperation) {
@@ -169,7 +186,16 @@ router.patch('/recipes/:id', auth, async (req, res) => {
             return res.status(401).send({ error: 'You are not authorized to update this recipe.' });
         }
 
-        updates.forEach((update) => (recipe[update] = req.body[update]));
+        updates.forEach((update) => {
+            if (update === 'reviews') {
+                recipe.reviews.push(req.body.reviews);
+
+                const totalValuation = recipe.reviews.reduce((acc, review) => acc + review.valuation, 0);
+                recipe.valuation = totalValuation / recipe.reviews.length;
+            } else {
+                recipe[update] = req.body[update];
+            }
+        });
         await recipe.save();
 
         res.send(recipe);
